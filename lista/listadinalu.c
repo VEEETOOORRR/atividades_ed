3080ef226c006f6ccb98180047355189f24f29f7
VEEETOOORRR <antonni2601.ag@gmail.com>
2023-10-26 09:26:28 -0300

    primeiro commit

---
+++ lista/listadinalu.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "listadinalu.h"
+
+void cria(ListaAluno *la){
+
+
+}
---
+++ lista/listadinalu.h
@@ -0,0 +1,25 @@
+typedef struct aluno{
+    int matricula;
+    char nome[30];
+    float nota1, nota2;
+} Aluno;
+
+typedef struct no{
+    Aluno dado;
+    struct no *prox;
+} No;
+
+typedef struct lista{
+    No *inicio;
+}ListaAluno;
+
+//operações básicas:
+
+void cria(ListaAluno *la);
+int tamanho(ListaAluno *la);
+int vazia(ListaAluno *la);
+int buscaPosMat(ListaAluno *la, int mat, int *pos);
+int buscaAlunoPos(ListaAluno *la, int pos, Aluno al);
+int insereFinal(ListaAluno *la, Aluno al);
+int removeAlunoMat(ListaAluno *la, int mat);
+void exibirALunos(ListaAluno *la);
